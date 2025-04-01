201472c72a3ffa4bb7681e427fabf7855005557d
rsclarke <hey@rsclarke.dev>
2025-04-01 12:27:52 +0800

    Upgrade to cloudflare v5 (#10)

--- main.tf
+++ main.tf
@@ -3,58 +3,66 @@ locals {
   policy_id = md5(local.policy)
 }
 
-resource "cloudflare_record" "smtp_tls" {
+resource "cloudflare_dns_record" "smtp_tls" {
   zone_id = var.zone_id
   name    = "_smtp._tls"
   type    = "TXT"
-  value   = "v=TLSRPTv1; rua=${join(",", var.rua)}"
+  content = "v=TLSRPTv1; rua=${join(",", var.rua)}"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "mta_sts" {
+resource "cloudflare_dns_record" "mta_sts" {
   zone_id = var.zone_id
   name    = "_mta-sts"
   type    = "TXT"
-  value   = "v=STSv1; id=${local.policy_id}"
+  content = "v=STSv1; id=${local.policy_id}"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "a" {
+resource "cloudflare_dns_record" "a" {
   zone_id = var.zone_id
   name    = "mta-sts"
   type    = "A"
-  value   = "192.0.2.1"
+  content = "192.0.2.1"
   proxied = true
+  ttl     = 1
 }
 
-resource "cloudflare_record" "aaaa" {
+resource "cloudflare_dns_record" "aaaa" {
   zone_id = var.zone_id
   name    = "mta-sts"
   type    = "AAAA"
-  value   = "100::"
+  content = "100::"
   proxied = true
+  ttl     = 1
 }
 
 resource "cloudflare_workers_kv_namespace" "mta_sts" {
-  title = "mta-sts.${var.zone_name}"
+  account_id = var.account_id
+  title      = "mta-sts.${var.zone_name}"
 }
 
 resource "cloudflare_workers_kv" "mta_sts" {
+  account_id   = var.account_id
   namespace_id = cloudflare_workers_kv_namespace.mta_sts.id
-  key          = "policy"
+  key_name     = "policy"
   value        = local.policy
 }
 
-resource "cloudflare_worker_script" "mta_sts_policy" {
-  name    = "mta-sts-${replace(var.zone_name, ".", "-")}"
-  content = file("${path.module}/mta_sts.js")
+resource "cloudflare_workers_script" "mta_sts_policy" {
+  account_id  = var.account_id
+  script_name = "mta-sts-${replace(var.zone_name, ".", "-")}"
+  content     = file("${path.module}/mta_sts.js")
 
-  kv_namespace_binding {
+  bindings = [{
     name         = "POLICY_NAMESPACE"
+    type         = "kv_namespace"
     namespace_id = cloudflare_workers_kv_namespace.mta_sts.id
-  }
+  }]
 }
 
-resource "cloudflare_worker_route" "mta_sts" {
-  zone_id     = var.zone_id
-  pattern     = "mta-sts.${var.zone_name}/*"
-  script_name = cloudflare_worker_script.mta_sts_policy.name
+resource "cloudflare_workers_route" "mta_sts" {
+  zone_id = var.zone_id
+  pattern = "mta-sts.${var.zone_name}/*"
+  script  = cloudflare_workers_script.mta_sts_policy.script_name
 }
